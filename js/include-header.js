fb481f040667a105c0302c856db96db06fd631e0
beka <beqacaava1997@gmail.com>
2025-04-13 23:59:45 +0400

    changeee

--- js/include-header.js
+++ js/include-header.js
@@ -1,5 +1,12 @@
+﻿// js/include-header.js
 fetch("../components/header.html")
     .then(res => res.text())
     .then(data => {
         document.getElementById("header").innerHTML = data;
+
+        // Dynamically inject the header CSS
+        const headerStyle = document.createElement("link");
+        headerStyle.rel = "stylesheet";
+        headerStyle.href = "../css/header.css"; // ✅ Adjust path based on current file location
+        document.head.appendChild(headerStyle);
     });
