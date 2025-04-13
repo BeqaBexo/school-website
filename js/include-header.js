7fd5f25b0ebf2fbcf82a0cacfdfa8d6fb4bfdbed
beka <beqacaava1997@gmail.com>
2025-04-13 23:22:26 +0400

    correct all

--- js/include-header.js
+++ js/include-header.js
@@ -1,12 +1,4 @@
-// include-header.js
-
-const path = window.location.pathname;
-
-// If you're inside the /pages folder (e.g. /pages/admission.html)
-const headerPath = path.includes("/pages/") ? "../components/header.html" : "components/header.html";
-
-// Load header
-fetch(headerPath)
+fetch("components/header.html")
     .then(res => res.text())
     .then(data => {
         document.getElementById("header").innerHTML = data;
