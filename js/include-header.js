889228be3b67e621aaaf1e4510dcc6f242c624fe
beka <beqacaava1997@gmail.com>
2025-04-13 23:18:30 +0400

    change sc

--- js/include-header.js
+++ js/include-header.js
@@ -1,4 +1,12 @@
-fetch("components/header.html")
+// include-header.js
+
+const path = window.location.pathname;
+
+// If you're inside the /pages folder (e.g. /pages/admission.html)
+const headerPath = path.includes("/pages/") ? "../components/header.html" : "components/header.html";
+
+// Load header
+fetch(headerPath)
     .then(res => res.text())
     .then(data => {
         document.getElementById("header").innerHTML = data;
